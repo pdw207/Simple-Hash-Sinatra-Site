4f3cf3444e1cf7ccd14078f23f715045414e9a4a
pdw207 <pdw207@gmail.com>
2014-03-01 13:15:47 -0500

    Phase 1 complete

---
+++ readme.txt
@@ -0,0 +1,13 @@
+Acceptance Criteria:
+
+1 I can navigate to a page /leaderboard to view the summary of the league. Each team is displayed on this page.
+
+2 For each team, I can see how many wins and losses they have.
+
+3 Teams are sorted first by the number of wins they have (teams with more wins are ranked higher) and second by the number of losses they have (teams with more losses are ranked lower).
+
+4 I can navigate to /teams/Patriots to view the team's information. I can navigate to a similar URL for each team.
+
+5 I can see the team's total wins and losses.
+
+6 I can also see the results of each game they have played, including the name of the opposing team and the scores for each team.
--- server.rb
+++ server.rb
@@ -1,10 +1,92 @@
 require 'pry'
 require 'sinatra'
 
-get '/' do
+
+
+get '/leaderboard' do
+
+  data = [
+  {
+    home_team: "Patriots",
+    away_team: "Broncos",
+    home_score: 7,
+    away_score: 3
+  },
+  {
+    home_team: "Broncos",
+    away_team: "Colts",
+    home_score: 3,
+    away_score: 0
+  },
+  {
+    home_team: "Patriots",
+    away_team: "Colts",
+    home_score: 11,
+    away_score: 7
+  },
+  {
+    home_team: "Steelers",
+    away_team: "Patriots",
+    home_score: 7,
+    away_score: 21
+  }
+]
+
+  # Initatilize new stats instance
+  stats = {}
+
+  def update_winner_and_loser stats, winner, loser
+        stats[winner][:win] += 1
+        stats[winner][:total] += 1
+
+        stats[loser][:loss] += 1
+        stats[loser][:total] += 1
+  end
+
+  def array_to_hash array
+    array.inject({}) do |result, (key, value)|
+      result.merge!(key => value)
+    end
+  end
+
+  data.each do |game|
+
+    # Simplify our reference to team names
+    home_team = game[:home_team]
+    away_team = game[:away_team]
+
+    # Initialize team in stats hash unless already included
+    stats[home_team] = {win: 0 , loss: 0, total: 0} unless stats.include?(home_team)
+    stats[away_team] = {win: 0 , loss: 0, total: 0} unless stats.include?(away_team)
+
+    # Update stats based on game
+    if game[:home_score] > game[:away_score]
+      update_winner_and_loser stats, home_team, away_team
+    else
+      update_winner_and_loser stats, away_team, home_team
+    end
+
+  end
+
+  #sort by win status
+  stats_sorted_by_win = array_to_hash stats.sort_by {|k,v| v[:win] }.reverse
+
+  #sort by loss if win is the same
+  @stats =  stats_sorted_by_win.sort_by {|k,v| v[:loss] if v[:win] == v[:win]}
+
+
   erb :index
 end
 
+get '/teams/:team' do
+  games = []
+  data.each do |game|
+    if game[:home_team] == params[:team] || game[:away_team] == params[:team]
+    @games << game
+  end
+
+ erb :team
+end
 
 set :view, File.dirname(__FILE__) + '/views'
 set :public_folder, File.dirname(__FILE__) + '/public'
